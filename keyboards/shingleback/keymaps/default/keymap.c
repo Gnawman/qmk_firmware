30e13a7afd533490e8a913ee62188a3342b11b4b
gnawman <haroldlander95@gmail.com>
2023-05-02 13:18:57 +1000

    removed mod-tap shifts from keymap

--- keyboards/shingleback/keymaps/default/keymap.c
+++ keyboards/shingleback/keymaps/default/keymap.c
@@ -12,38 +12,36 @@
 #define I_GUI       LGUI_T(KC_I)
 #define S_ALT       LALT_T(KC_S)
 #define E_ALT       LALT_T(KC_E)
-#define T_SFT       LSFT_T(KC_T)
-#define N_SFT       LSFT_T(KC_N)
 #define QUOT_CTL    LCTL_T(KC_QUOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT(
-             KC_W,    KC_F,    KC_P,     KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,
-             R_GUI,   S_ALT,   T_SFT,    KC_G,                         KC_M,    N_SFT,   E_ALT,   I_GUI,
-    A_CTL,   KC_X,    KC_C,    KC_D,                                            KC_H,    KC_K,    KC_V,     O_CTL,
-    KC_Q,                      ENT_LAY1, KC_LSFT, KC_BTN1,    KC_TAB,  KC_SPC,  MO(2),                      KC_Z
+             KC_W,   KC_F,   KC_P,     KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,
+             R_GUI,  S_ALT,  KC_T,     KC_G,                         KC_M,    KC_N,    E_ALT,   I_GUI,
+    A_CTL,   KC_X,   KC_C,   KC_D,                                            KC_H,    KC_K,    KC_V,    O_CTL,
+    KC_Q,                    ENT_LAY1, KC_LSFT, KC_BTN1,    KC_TAB,  KC_SPC,  MO(2),                     KC_Z
   ),
 
   [1] = LAYOUT(
-             KC_7,    KC_8,    KC_9,     _______,                      KC_LBRC, KC_SCLN, KC_SLSH, KC_BSLS,
-             KC_4,    KC_5,    KC_6,     KC_0,                         KC_RBRC, KC_COMM, KC_DOT,  KC_MINS,
-    KC_BSPC, KC_1,    KC_2,    KC_3,                                           KC_LPRN, KC_RPRN, KC_EQL,    QUOT_CTL,
-    KC_DEL,                    _______,  _______, _______,    _______, _______, _______,                    KC_GRV
+             KC_7,   KC_8,   KC_9,     _______,                      KC_LBRC, KC_SCLN, KC_SLSH, KC_BSLS,
+             KC_4,   KC_5,   KC_6,     KC_0,                         KC_RBRC, KC_COMM, KC_DOT,  KC_MINS,
+    KC_BSPC, KC_1,   KC_2,   KC_3,                                            KC_LPRN, KC_RPRN, KC_EQL,  QUOT_CTL,
+    KC_DEL,                  _______,  _______, _______,    _______, _______, _______,                   KC_GRV
   ),
 
   [2] = LAYOUT(
-             KC_F7,   KC_F8,   KC_F9,    KC_F10,                       _______, KC_HOME, KC_UP,   KC_END,
-             KC_F4,   KC_F5,   KC_F6,    KC_F11,                       KC_ESC,  KC_LEFT, KC_DOWN, KC_RIGHT,
-    KC_F12,  KC_F1,   KC_F2,   KC_F3,                                           KC_BTN1, KC_APP,  KC_BTN2,  _______,
-    KC_F13,                    _______,  _______, _______,    _______, _______, _______,                    TG(3)
+             KC_F7,  KC_F8,  KC_F9,    KC_F10,                       _______, KC_HOME, KC_UP,   KC_END,
+             KC_F4,  KC_F5,  KC_F6,    KC_F11,                       KC_ESC,  KC_LEFT, KC_DOWN, KC_RIGHT,
+    KC_F12,  KC_F1,  KC_F2,  KC_F3,                                           KC_BTN1, KC_APP,  KC_BTN2,  _______,
+    KC_F13,                  _______,  _______, _______,    _______, _______, _______,                    TG(3)
   ),
 
   [3] = LAYOUT(
-             KC_1,    KC_2,    KC_3,     KC_4,                         _______, _______, _______, _______,
-             KC_Q,    KC_E,    KC_F,     KC_SPC,                       _______, _______, _______, _______,
-    KC_LSFT, KC_C,    KC_V,    KC_R,                                            _______, _______, _______,  _______,
-    KC_LCTL,                   KC_ESC,   KC_ENT,  KC_TAB,     _______, _______, _______,                    TG(3)
+             KC_1,   KC_2,   KC_3,     KC_4,                         _______, _______, _______, _______,
+             KC_Q,   KC_E,   KC_F,     KC_SPC,                       _______, _______, _______, _______,
+    KC_LSFT, KC_C,   KC_V,   KC_R,                                            _______, _______, _______,  _______,
+    KC_LCTL,                 KC_ESC,   KC_ENT,  KC_TAB,     _______, _______, _______,                    TG(3)
   )
 
 };
