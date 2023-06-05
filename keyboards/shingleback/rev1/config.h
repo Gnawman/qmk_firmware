48bb712de33b903cf2b6d96f3ae6dfc599699d06
gnawman <haroldlander95@gmail.com>
2023-06-05 21:58:38 +1000

    rev2 initial commit

--- keyboards/shingleback/rev1/config.h
+++ keyboards/shingleback/rev1/config.h
@@ -1,12 +1,14 @@
 #pragma once
 
 /* key matrix size */
-#define MATRIX_ROWS 5
+#define MATRIX_ROWS 4
 #define MATRIX_COLS 8
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { D1, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { B1, B3, B2, B6, F4, F5, F6, F7 }
+#define MATRIX_ROW_PINS { B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4, B5 }
+
+#define DIP_SWITCH_PINS { F4, F7 }
 
 #define ENCODERS_PAD_A { D3 }
 #define ENCODERS_PAD_B { D2 }
