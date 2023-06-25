0dc540bc3da223b57579c930dc9fc5e1ba83e949
gnawman <haroldlander95@gmail.com>
2023-06-25 17:28:52 +1000

    doing encoders the new way

--- keyboards/shingleback/config.h
+++ keyboards/shingleback/config.h
@@ -8,11 +8,6 @@
 #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { B1, B3, B2, B6, F4, F5, F6, F7 }
 
-#define ENCODERS_PAD_A { D3 }
-#define ENCODERS_PAD_B { D2 }
-
-#define ENCODER_RESOLUTION 4
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
