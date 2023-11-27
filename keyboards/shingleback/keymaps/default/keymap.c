28841f4f02919f6cfb77f168d936faa763624a38
gnawman <haroldlander95@gmail.com>
2023-11-27 17:03:45 +1100

    added right hand to gaming layer so Ican play risk of rain

--- keyboards/shingleback/keymaps/default/keymap.c
+++ keyboards/shingleback/keymaps/default/keymap.c
@@ -42,10 +42,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [4] = LAYOUT(
-             KC_1,    KC_2,    KC_3,    KC_4,                           _______, _______, _______, _______,
-    KC_LSFT, KC_Q,    KC_E,    KC_SPC,  KC_F,                           _______, _______, _______, _______, _______,
-    KC_LCTL, KC_C,    KC_V,    KC_R,    KC_MPLY,                                 _______, _______, _______, _______,
- G(S(KC_S)), TG(4),   TG(3),   KC_ENT,  KC_I,     KC_ESC,      _______, _______, _______
+             KC_Q,    KC_W,    KC_E,    KC_R,                           KC_P,    KC_HOME, KC_UP,   KC_END,
+    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                           KC_ESC,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_5,
+    KC_LCTL, KC_C,    KC_V,    KC_R,    KC_MPLY,                                 KC_1,    KC_2,    KC_3,     KC_4,
+ G(S(KC_S)), TG(4),   TG(3),   KC_ENT,  KC_SPC,     KC_TAB,    KC_L,    KC_M,    KC_N
   )
 
 };
